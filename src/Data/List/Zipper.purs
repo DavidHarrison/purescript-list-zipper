2bfe7ba527ed4e769fa5c01733e78e50cfa6464b
David Harrison <djrh777@gmail.com>
2015-07-10 22:07:08 -0700

    Documentation and `Unfoldable` instance for `Compose Maybe Zipper`

--- src/Data/List/Zipper.purs
+++ src/Data/List/Zipper.purs
@@ -7,35 +7,52 @@ module Data.List.Zipper
 
 import Prelude
 
-import Control.Comonad  (Comonad, extract)
-import Control.Extend   (Extend, extend)
-import Data.List        (List(..), head, reverse, snoc, tail, toList)
-import Data.Foldable    (Foldable, foldl, foldMap, foldr, intercalate)
-import Data.Maybe       (Maybe(..), maybe)
-import Data.Traversable (Traversable, sequence, traverse)
-import Data.Tuple       (Tuple(Tuple))
-import Data.Unfoldable  (Unfoldable, unfoldr)
+import Control.Comonad      (Comonad, extract)
+import Control.Extend       (Extend, extend)
+import Data.List            (List(..), head, reverse, snoc, tail, toList)
+import Data.Foldable        (Foldable, foldl, foldMap, foldr, intercalate)
+import Data.Functor.Compose (Compose(..))
+import Data.Maybe           (Maybe(..), maybe)
+import Data.Traversable     (Traversable, sequence, traverse)
+import Data.Tuple           (Tuple(Tuple))
+import Data.Unfoldable      (Unfoldable, unfoldr)
 
 data Zipper a = Zipper (List a) a (List a)
 
+-- | O(1) Move one step closer to the start of the Zipper.
+-- | This is the inverse of `down` where their composition is defined
+-- | (while not at one of the ends of the Zipper).
 up :: forall a. Zipper a -> Maybe (Zipper a)
 up (Zipper ls c rs) = Zipper <$> ls' <*> c' <*> rs'
   where ls' = tail ls
         c'  = head ls
         rs' = pure (Cons c rs)
 
+-- | O(1) Move one step closer to the end of the Zipper.
+-- | This is the inverse of `up` where their composition is defined
+-- | (while not at one of the ends of the Zipper).
 down :: forall a. Zipper a -> Maybe (Zipper a)
 down (Zipper ls c rs) = Zipper <$> ls' <*> c' <*> rs'
   where ls' = pure (Cons c ls)
         c'  = head rs
         rs' = tail rs
 
+-- | O(n) Go to the beginning of the Zipper.
+-- | This should be an idempotent operation of the Zipper (once at the
+-- | beginning, moving to the head will not change the focus).
 beginning :: forall a. Zipper a -> Zipper a
 beginning = lastJust up
 
+-- | O(n) Go to the end of the Zipper.
+-- | This should be an idempotent operation of the Zipper (once at the end,
+-- | moving to the head will not change the focus).
 end :: forall a. Zipper a -> Zipper a
 end = lastJust down
 
+-- | Convert a Zipper into any type with an Unfoldable instance. Assuming
+-- | that the definition of `unfoldr` is O(n), the unfolding will be
+-- | O(2 * l + r) where l is the length of the lefthand/top list of the Zipper
+-- | and r is the length of the righthand/bottom.
 toUnfoldable :: forall a f. (Unfoldable f) => Zipper a -> f a
 toUnfoldable (Zipper ls c rs) = unfoldr iter $ Tuple (reverse ls) (Cons c rs)
   where iter :: (Tuple (List a) (List a)) -> Maybe (Tuple a (Tuple (List a) (List a)))
@@ -43,6 +60,9 @@ toUnfoldable (Zipper ls c rs) = unfoldr iter $ Tuple (reverse ls) (Cons c rs)
         iter (Tuple Nil (Cons b bs)) = Just $ Tuple b $ Tuple Nil bs
         iter (Tuple Nil Nil)         = Nothing
 
+-- | Convert any type with a Foldable instance into a Zipper with the
+-- | possibility for failure (in the case of an empty Foldable). Assuming the
+-- | definition of `foldr` is O(n), the folding will also be O(n).
 fromFoldable :: forall a f. (Foldable f) => f a -> Maybe (Zipper a)
 fromFoldable x = Zipper Nil <$> head asList <*> tail asList
   where asList = toList x
@@ -51,7 +71,7 @@ fromFoldable x = Zipper Nil <$> head asList <*> tail asList
 -- Instances ------------------------------------------------------------------
 -------------------------------------------------------------------------------
 
--- show the zipper with the focus in curly-braces ({})
+-- | Show the Zipper as a list with the focus in curly-braces ({}).
 instance showZipper :: (Show a) => Show (Zipper a) where
     -- show :: forall a. (Show a) => Zipper a -> String
     show (Zipper ls c rs) = "[" <> intercalate ", " (ls' <> Cons c' rs') <> "]"
@@ -59,16 +79,22 @@ instance showZipper :: (Show a) => Show (Zipper a) where
             c'  = "{" <> show c <> "}"
             rs' = map show rs
 
--- NOTE: the focus is part of the equality for the Zipper
+-- | Check the equality of two Zippers.
+-- | NOTE: the focus is part of the equality.
 instance eqZipper :: (Eq a) => Eq (Zipper a) where
     -- eq :: forall a. (Eq a) => Zipper a -> Zipper a -> Bool
     eq (Zipper ls c rs) (Zipper ls' c' rs') =
         ls == ls' && c == c' && rs == rs'
 
+{- TODO: Comparison is complicated by the position of the cursor, I am not sure
+   what the correct implementation is (although the following seems to be
+   law abiding), so I will leave it commented for now.
+-- | Compare two Zippers starting from their left-/top-most element.
 instance ordZipper :: (Ord a) => Ord (Zipper a) where
     -- compare :: forall a. (Ord a) => Zipper a -> Zipper a -> Ordering
     compare (Zipper ls c rs) (Zipper ls' c' rs') =
-        compare ls ls' <> compare c c' <> compare rs rs'
+        compare (reverse ls) (reverse ls') <> compare c c' <> compare rs rs'
+-}
 
 instance functorZipper :: Functor Zipper where
     -- map :: forall a b. (a -> b) -> Zipper a -> Zipper b
@@ -103,9 +129,16 @@ instance traversableZipper :: Traversable Zipper where
     -- sequence :: forall a m. (Applicative m) => (Zipper (m a)) -> m (Zipper a)
     sequence = traverse id
 
+instance unfoldableMaybeZipper :: Unfoldable (Compose Maybe Zipper) where
+    unfoldr f x = Compose $ Zipper Nil <$> head asList <*> tail asList
+      where asList = unfoldr f x
+
 -------------------------------------------------------------------------------
 -- Helper Functions -----------------------------------------------------------
 -------------------------------------------------------------------------------
+
+-- Iteratively applies the provided function until it reaches a `Nothing` and
+-- returns the last `Just` value.
 lastJust :: forall a. (a -> Maybe a) -> a -> a
 lastJust f x = maybe x (lastJust f) $ f x
 
