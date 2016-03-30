789155a44498e4716ef71eebffdd88683af9c4a4
Ryan Rempel <rgrempel@gmail.com>
2016-03-30 16:47:36 -0500

    Update tests to compile without error on Purescript 0.8.3.

--- test/Main.purs
+++ test/Main.purs
@@ -30,50 +30,62 @@ import Test.QuickCheck.Laws.Data.Functor        (checkFunctor)
 -- import Test.QuickCheck.Laws.Data.Ord            (checkOrd)
 import Type.Proxy                               (Proxy(Proxy), Proxy2(Proxy2))
 
-import Data.List.Zipper ( Zipper(..)
-                        , up, down
-                        , beginning, end
-                        , fromFoldable, toUnfoldable
-                        )
+import Data.List.Zipper (Zipper(..))
+
+-- Qualified, since we'll need to wrap them for ArbitraryZipper
+import Data.List.Zipper
+    ( up, down
+    , beginning, end
+    , fromFoldable, toUnfoldable
+    ) as Zipper
+
+import Control.Extend (Extend, extend)
+import Control.Comonad (Comonad, extract)
+import Data.Unfoldable (Unfoldable)
+
 
 main = do
     log "Checking that `show` is total"
-    quickCheck $ const Success $ show :: Zipper Number -> String
+    quickCheck $ const Success $ show :: ArbitraryZipper Number -> String
+
     checkEq      prxZipper
     -- checkOrd     prxZipper
-    checkFunctor prxZipper2 prxA prxB
-    checkExtend  prxZipper2 prxA prxB prxC
-    checkComonad prxZipper2 prxA prxB
+    checkFunctor prxZipper2
+    checkExtend  prxZipper2
+    checkComonad prxZipper2
+
     log "Checking that folding is preserved by natural transformations to an element and order preserving structure"
-    quickCheck $ \f -> (===) <$> foldMap (f :: Int -> String) <*> foldMap f <<< (toUnfoldable :: Zipper Int -> Array Int)
-    quickCheck $ \f i -> (===) <$> foldr (f :: Int -> String -> String) i <*> foldr f i <<< (toUnfoldable :: Zipper Int -> Array Int)
-    quickCheck $ \f i -> (===) <$> foldl (f :: String -> Int -> String) i <*> foldl f i <<< (toUnfoldable :: Zipper Int -> Array Int)
+    quickCheck $ \f -> (===) <$> foldMap (f :: Int -> String) <*> foldMap f <<< (toUnfoldable :: ArbitraryZipper Int -> Array Int)
+    quickCheck $ \f i -> (===) <$> foldr (f :: Int -> String -> String) i <*> foldr f i <<< (toUnfoldable :: ArbitraryZipper Int -> Array Int)
+    quickCheck $ \f i -> (===) <$> foldl (f :: String -> Int -> String) i <*> foldl f i <<< (toUnfoldable :: ArbitraryZipper Int -> Array Int)
+
     -- TODO: Uncomment when Traversable laws are merged into
     --       purescript-quickcheck-laws
     -- checkTraversable prxZipper2 prxF2 prxG2 prxA prxB prxC
     log "Checking that composition of fromFoldable and toUnfoldable preserve elements and order"
     quickCheck $ notUnequal (Just <<< beginning)
                $ map beginning <<< fromFoldable
-                               <<< (toUnfoldable :: Zipper Number -> Array Number)
+                               <<< (toUnfoldable :: ArbitraryZipper Number -> Array Number)
     -- TODO: assure that we exclude information loss (as with beginning above)
     --       in the other Foldable/Unfoldable (does not matter for Array)
     quickCheck $ notUnequal Just
-               $ return <<< (toUnfoldable :: Zipper Number -> Array Number)
+               $ return <<< (toUnfoldable :: ArbitraryZipper Number -> Array Number)
                         <=< fromFoldable
     log "Checking that `up` and `down` are inverses where their composition is defined"
-    quickCheck $ notUnequal Just $ (up <=< down) :: Zipper Number -> Maybe (Zipper Number)
-    quickCheck $ notUnequal Just $ (down <=< up) :: Zipper Number -> Maybe (Zipper Number)
+    quickCheck $ notUnequal Just $ (up <=< down) :: ArbitraryZipper Number -> Maybe (ArbitraryZipper Number)
+    quickCheck $ notUnequal Just $ (down <=< up) :: ArbitraryZipper Number -> Maybe (ArbitraryZipper Number)
     log "Checking that `beginning` and `end` are idempotent"
-    quickCheck $ idempotent $ beginning :: Zipper Number -> Zipper Number
-    quickCheck $ idempotent $ end       :: Zipper Number -> Zipper Number
+    quickCheck $ idempotent $ beginning :: ArbitraryZipper Number -> ArbitraryZipper Number
+    quickCheck $ idempotent $ end       :: ArbitraryZipper Number -> ArbitraryZipper Number
 
-prxZipper2 :: Proxy2 Zipper
+
+prxZipper2 :: Proxy2 ArbitraryZipper
 prxZipper2 =  Proxy2
 -- prxF2 :: Proxy2 Array
 -- prxF2 = Proxy2
 -- prxG2 :: Proxy2 Maybe
 -- prxG2 = Proxy2
-prxZipper :: Proxy (Zipper Number)
+prxZipper :: Proxy (ArbitraryZipper Number)
 prxZipper = Proxy
 prxA :: Proxy Boolean
 prxA = Proxy
@@ -88,14 +100,56 @@ notUnequal f g x = maybe Success id $ (===) <$> f x <*> g x
 idempotent :: forall a. (Eq a) => (a -> a) -> a -> Boolean
 idempotent f = eq <$> f <<< f <*> f
 
-instance arbList :: (Arbitrary a) => Arbitrary (List a) where
-    -- arbitrary :: forall a. (Arbitrary a) => Gen (List a)
-    arbitrary = toList <$> (arbitrary :: Gen (Array a))
+-- We can't make an arbitrary instance here for Zipper itself, because of orphan instances.
+-- We could make one in the main module, but then we'd pull in Quickcheck in production code,
+-- which isn't really ideal. So, we actually test a newtype, and make the necessary instances
+-- here, so that we're really testing Zipper.
+newtype ArbitraryZipper a = ArbitraryZipper (Zipper a)
 
-instance coarbFoldable :: (Foldable f, Coarbitrary a) => Coarbitrary (f a) where
-    -- coarbitrary :: forall a r. (Foldable f, Coarbitrary a) => (f a) -> Gen r -> Gen r
-    coarbitrary = foldl (\f x -> f <<< coarbitrary x) id
-
-instance arbZipper :: (Arbitrary a) => Arbitrary (Zipper a) where
+instance arbArbitraryZipper :: (Arbitrary a) => Arbitrary (ArbitraryZipper a) where
     -- arbitrary :: forall a. (Arbitrary a) => Gen (Zipper a)
-    arbitrary = Zipper <$> arbitrary <*> arbitrary <*> arbitrary
+    arbitrary = ArbitraryZipper <$> (Zipper <$> arbitrary <*> arbitrary <*> arbitrary)
+
+instance coarbArbitraryZipper :: (Coarbitrary a) => Coarbitrary (ArbitraryZipper a) where
+    coarbitrary (ArbitraryZipper (Zipper left middle right)) = coarbitrary left >>> coarbitrary middle >>> coarbitrary right
+
+-- Wrapping Zipper functions for ArbitraryZipper
+up :: forall a. ArbitraryZipper a -> Maybe (ArbitraryZipper a)
+up (ArbitraryZipper zipper) = ArbitraryZipper <$> Zipper.up zipper
+
+down :: forall a. ArbitraryZipper a -> Maybe (ArbitraryZipper a)
+down (ArbitraryZipper zipper) = ArbitraryZipper <$> Zipper.down zipper
+
+beginning :: forall a. ArbitraryZipper a -> ArbitraryZipper a
+beginning (ArbitraryZipper zipper) = ArbitraryZipper $ Zipper.beginning zipper
+
+end :: forall a. ArbitraryZipper a -> ArbitraryZipper a
+end (ArbitraryZipper zipper) = ArbitraryZipper $ Zipper.end zipper
+
+toUnfoldable :: forall a f. (Unfoldable f) => ArbitraryZipper a -> f a
+toUnfoldable (ArbitraryZipper zipper) = Zipper.toUnfoldable zipper
+
+fromFoldable :: forall a f. (Foldable f) => f a -> Maybe (ArbitraryZipper a)
+fromFoldable f = ArbitraryZipper <$> Zipper.fromFoldable f
+
+-- Purescript will someday be able to derive most of the rest of these ...
+-- see https://github.com/purescript/purescript/issues/514.
+instance showArbitraryZipper :: (Show a) => Show (ArbitraryZipper a) where
+    show (ArbitraryZipper zipper) = show zipper
+
+instance eqArbitraryZipper :: (Eq a) => Eq (ArbitraryZipper a) where
+    eq (ArbitraryZipper zipper1) (ArbitraryZipper zipper2) = eq zipper1 zipper2
+
+instance functorArbitraryZipper :: Functor ArbitraryZipper where
+    map func (ArbitraryZipper zipper) = ArbitraryZipper $ map func zipper
+
+instance extendArbitraryZipper :: Extend ArbitraryZipper where
+    extend func (ArbitraryZipper zipper) = ArbitraryZipper $ extend (func <<< ArbitraryZipper) zipper
+
+instance comonadArbitraryZipper :: Comonad ArbitraryZipper where
+    extract (ArbitraryZipper zipper) = extract zipper
+
+instance foldableArbitraryZipper :: Foldable ArbitraryZipper where
+    foldr func memo (ArbitraryZipper zipper) = foldr func memo zipper
+    foldl func memo (ArbitraryZipper zipper) = foldl func memo zipper
+    foldMap func (ArbitraryZipper zipper) = foldMap func zipper
